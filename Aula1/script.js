6e3b4b008b5033c866380d5db2428e92ea18ee9a
KelvinLRusch <kelvinrusch@hotmail.com>
2024-10-03 21:55:09 -0300

    fim de aula

--- Aula1/script.js
+++ Aula1/script.js
@@ -9,9 +9,9 @@
 //$ esta chamando o JQUERY
 
 $(document).ready(function(){
-    // Define a variável "resposta" como variável global
-    // para que ela seja acessível à partir de qualquer função
-    let resposta = '';
+    /* Define a variável "resposta" como variável global
+    para que ela seja acessível à partir de qualquer função*/
+    let resposta = false;
     
     //adiciona o pattern ao campo do CEP
     $("input[name=cep]").mask("00000-000");
@@ -26,6 +26,19 @@ $(document).ready(function(){
         event.preventDefault();
     });
 
+    $("input[name=tel]").mask("(00) 00000-0000");
+    $("input[name=tel]").on("keyup", function(){
+        let tel = $("input[name=tel]").val();
+        tel = tel.replace(/[\D]/g, '');
+        if(tel.length < 11) {
+            maskara = "(00) 0000-00000";
+        } else {
+            maskara = "(00) 00000-0000";
+        }
+
+        $("input[name=tel]").mask(maskara);
+    })
+
     //chama a funçao ao cep ser alterado
     $("input[name=cep]").on("keyup", function(event){
         //passa o valor do input para a variavel
@@ -37,22 +50,47 @@ $(document).ready(function(){
         //validação de quantidade de caracteres
         if (cep.length == 8) {
             //alert(cep);
-            $.ajax("https://viacep.com.br/ws/" + cep + "/json").done(function(data){
+            $.ajax("https://viacep.com.br/ws/" + cep + "/json")
+            .done(function(data){
                 resposta = JSON.parse(data);
                 if(!resposta.erro){
                     $("input[name=cep]").removeClass("is-invalid");
                     $("input[name=rua]").val(resposta.logradouro);
+                    if(resposta.logradouro !== ""){
+                        $("input[name=rua]").prop("disabled",true);
+                    }
                     $("input[name=complemento]").val(resposta.complemento);
                     $("input[name=bairro]").val(resposta.bairro);
+                    if(resposta.bairro !== ""){
+                        $("input[name=bairro]").prop("disabled",true);
+                    }
                     $("select[name=estado]").val(resposta.uf);
                     $("select[name=estado]").trigger("change");
+                    $("select[name=estado]").prop("disabled",true);
+                    $("select[name=cidade]").prop("disabled",true);
                 }
                 
+            })
+            .fail(function(data){
+                respota = {"erro": "true"}
             });
+            
         }else if (cep.length == 0){
             $("input[name=cep]").removeClass("is-invalid");
         }else{
             $("input[name=cep]").addClass("is-invalid");
+            $("select[name=estado]").val("");
+            $("select[name=cidade]").val("");
+            $("input[name=rua]").val("");
+            $("input[name=bairro]").val("");
+
+            $("select[name=estado]").prop("disabled",false);
+            $("select[name=cidade]").prop("disabled",false);
+            $("input[name=rua]").prop("disabled",false);
+            $("input[name=bairro]").prop("disabled",false);
+
+            $("select[name=cidade]").empty();
+            $("select[name=cidade]").append('<option value="">Primeiro selecione o estado</option>');
         }
     });
 
@@ -89,7 +127,7 @@ $(document).ready(function(){
             });
         }else{
             $("select[name=cidade]").empty();
-            $("select[name=cidade]").append('<option value="">Primeiro selecione a cidade</option>');
+            $("select[name=cidade]").append('<option value="">Primeiro selecione o estado</option>');
         }
     })
 });
