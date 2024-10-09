32b3efa131ae64000c0c7786a964b088ae140e2e
KelvinLRusch <kelvinrusch@hotmail.com>
2024-10-08 22:01:19 -0300

    Fim de aula

--- Aula2/script.js
+++ Aula2/script.js
@@ -19,6 +19,19 @@ $(document).ready(function(){
                 let friend = data2.friends[x].firstName + " " + data2.friends[x].lastName;
                 $("#resultado2").append("Amigo " + (x+1) + ": " + friend + "<br>");
             }
+
+            $("#resultado2").append("Total de amigos do John: " + data2.friends.length);
+        });
+    });
+
+    $("#button3").on("click",function(event){
+        $.getJSON("./data3.json", function(data3){
+            for (var x in data3.results) {
+                $("#resultado3").append(data3.results[x].gender + "<br>");
+                $("#resultado3").append(data3.results[x].name.first + "<br>");
+                $("#resultado3").append(data3.results[x].email + "<br>");
+            }
+            $("#resultado3").append(data3.info.seed + "<br>");
         });
     });
 
